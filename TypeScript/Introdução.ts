8b778d135bd30f9d14da5af28e63332fb48a112c
Ghost_ <yukideluar@gmail.com>
2025-05-07 08:04:41 -0300

    Create Introdução.ts

---
+++ TypeScript/Introdução.ts
@@ -0,0 +1,27 @@
+//tipo basicos: number, string e boolean(lógico)
+// tipo boolean
+let x: boolean;
+x = true;
+
+while(x)
+{
+    x = false;
+}
+
+//podemos fazer tudo em javascript! tipo any: qualquer coisa
+let y;
+y = 3
+y = "oi";
+
+//let-else: mesma coisa que javascript
+let z: number;
+z = 40;
+//and,or e not: respectivamentre &&, || e !
+if(z >= 0 && z <=100)
+{
+    console.log("Está dentro do intervalo entre 0 a 100");
+}
+else
+{
+    console.log("Está fora")
+}
