c05d3fddacbcb036dcbe545980857e47852f99e6
Ghost_ <yukideluar@gmail.com>
2025-05-21 08:33:51 -0300

    Update 21-05-25-ex1.ts

--- TypeScript/21-05-25-ex1.ts
+++ TypeScript/21-05-25-ex1.ts
@@ -1,5 +1,6 @@
 //Criar a classe Ponto3D. Ela deve ter os atributos públicos x,y e z, e um construtor para inicia-los.
-
+// Criar a classe ListaDePontos, a qual encapsula uma lista de Ponto3D. Assim, a lista deve ser um atributo private. Não precisa de construtor, já que podemos inicializar a lista vazia em sua definição
+// Criar método getPonto. O método tem como argumento o índice i do elemento a ser retornado, e retorna o i-ésimo ponto da lista. Crie também o método inserePonto. Ele recebe um novo ponto como argumento, e insere no final da lista.
 class Ponto3D 
 {
   x: number;
@@ -12,4 +13,9 @@ class Ponto3D
     this.y = y;
     this.z = z;
   }
+}
+
+class ListaDePontos 
+{
+  private ldp: Ponto3D[] = []; //array (lista de pontos)
 }
