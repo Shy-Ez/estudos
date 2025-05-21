68226319d484db676b77bf71dbff5855e7e057b5
Ghost_ <yukideluar@gmail.com>
2025-05-21 08:26:56 -0300

    Update 21-05-25-ex1.ts

--- TypeScript/21-05-25-ex1.ts
+++ TypeScript/21-05-25-ex1.ts
@@ -1,2 +1,15 @@
 //Criar a classe Ponto3D. Ela deve ter os atributos públicos x,y e z, e um construtor para inicia-los.
 
+class Ponto3D 
+{
+  x: number;
+  y: number;
+  z: number;
+
+  constructor(x: number,y: number,z: number)
+  {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
